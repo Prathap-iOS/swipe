91f0d9ae13354a1ca44a11cb21193d64bb4c9e87
NowFloats <prathap.reddy@nowfloats.com>
2023-08-22 22:27:19 +0530

    Added done button to toolbar in a keyboard

--- swipe/View Controllers/AddProductViewController.swift
+++ swipe/View Controllers/AddProductViewController.swift
@@ -45,6 +45,7 @@ class AddProductViewController: UIViewController, UIImagePickerControllerDelegat
         super.viewDidLoad()
 
         self.productTypeList.removeDuplicates()
+        self.addDoneButtonOnKeyboard()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -257,6 +258,27 @@ class AddProductViewController: UIViewController, UIImagePickerControllerDelegat
         } else {
             self.productTypeTableView.isHidden = true
         }
+    }
+    
+    func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        productNameTF.inputAccessoryView = doneToolbar
+        sellingPriceTF.inputAccessoryView = doneToolbar
+        taxRateTF.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction(){
+        productNameTF.resignFirstResponder()
+        sellingPriceTF.resignFirstResponder()
+        taxRateTF.resignFirstResponder()
     }
 }
 
